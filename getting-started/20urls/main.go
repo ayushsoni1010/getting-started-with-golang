67ec1412f74ed5a20d6995b8a2b5e15721a1f1a5
Ayush Soni <mr.soni1002001@gmail.com>
2022-03-29 23:11:27 +0530

    handling requests in golang

---
+++ getting-started/20urls/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj456ghb"
+
+func main() {
+	fmt.Println("Welcome to handling URLs in Golang")
+	fmt.Println(myurl)
+
+	result, _ := url.Parse(myurl)
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Port())
+	fmt.Println(result.Path)
+	fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Printf("The type of query params are: %T\n", qparams)
+
+	fmt.Println(qparams["coursename"])
+	fmt.Println(qparams["paymentid"])
+
+	for _, val := range qparams {
+		fmt.Println("Param is: ", val)
+	}
+
+	partsOfUrl := &url.URL{
+		Scheme:   "https",
+		Host:     "lco.dev",
+		Path:     "/tutcss",
+		RawQuery: "user=ayush",
+	}
+
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
+}
